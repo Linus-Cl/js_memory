d6dd1fa52991426fec60e758f45e70f5ca77dffa
Linus Claußen <l.claussen@tu-braunschweig.de>
2023-10-11 16:18:48 +0200

    image flip test

--- src/memory.js
+++ src/memory.js
@@ -4,9 +4,15 @@ function setup() {
     console.log("setup");
     for (let i = 0; i < 40; i++) {
         const gridSquare = document.createElement('div');
-        gridSquare.classList.add('w-40', 'h-40', 'bg-primary', 'grid-square', 'hover:bg-accent', 'transition', 'transform', 'hover:scale-105');
+        gridSquare.id = i;
+        gridSquare.onclick = function () { flip(i); }
+        gridSquare.classList.add('card', 'w-40', 'h-40', 'bg-primary', 'grid-square', 'hover:bg-accent', 'transition', 'transform', 'hover:scale-105');
         gridContainer.appendChild(gridSquare);
     }
 }
 
 
+function flip(id) {
+    console.log("flip");
+    document.getElementById(id).className = "card w-40 h-40 bg-goat bg-cover bg-center bg-no-repeat grid square hover:transition transform hover:scale-105";
+}
