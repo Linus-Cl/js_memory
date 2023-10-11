0ac704f6e6a6b3e5f3688f320c97ea8f3623469b
Linus Claußen <l.claussen@tu-braunschweig.de>
2023-10-11 15:15:59 +0200

    added colortheme

--- src/memory.js
+++ src/memory.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const gridContainer = document.querySelector('.grid-cols-8');
+
+function setup() {
+    console.log("setup");
+    for (let i = 0; i < 40; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('w-40', 'h-40', 'bg-primary', 'grid-square', 'hover:bg-accent', 'transition', 'transform', 'hover:scale-105');
+        gridContainer.appendChild(gridSquare);
+    }
+}
+
+
