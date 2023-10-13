f2457008161f170e0821ee27378f3ade15a690d9
y0105877 <l.claussen@tu-braunschweig.de>
2023-10-13 16:15:55 +0200

    added rngCard Generator

--- src/memory.js
+++ src/memory.js
@@ -1,4 +1,9 @@
 const gridContainer = document.querySelector('.grid-cols-8');
+const randomCard = randomCardsGenerator(40);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
 function setup() {
     console.log("setup");
@@ -11,8 +16,32 @@ function setup() {
     }
 }
 
-
 function flip(id) {
     console.log("flip");
     document.getElementById(id).className = "card w-40 h-40 bg-goat bg-cover bg-center bg-no-repeat grid square hover:transition transform hover:scale-105";
+}
+
+
+/**
+ * 
+ *  [0 1 2 3 4 5 ...... 39]
+ * 
+ * randint(40) => 4
+ * arr.pop(4);
+ * randint(40) => 
+ * arr.pop(18);
+ * 
+ * 
+ */
+
+function* randomCardsGenerator(numberOfCards) {
+    let cards = new Set();
+
+    while (cards.size < numberOfCards) {
+        let randomIndex = Math.floor(Math.random() * numberOfCards);
+        if (!cards.has(randomIndex)) {
+            cards.add(randomIndex);
+            yield randomIndex;
+        }
+    }
 }
